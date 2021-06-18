750652be1790ff4d06a123a53814cd53b69e439e
manuelrafael-prog <77649049+manuelrafael-prog@users.noreply.github.com>
2021-06-18 15:56:47 +0800

    Challenge 22

---
+++ Programs/Python Practice/Challenge 22 - Happy Message.py
@@ -0,0 +1,8 @@
+message = int(input("How happy you feel on a scale between 1 and 10"))
+
+if message <= 3:
+    print("Don't be like that :)")
+elif message <= 7:
+    print("Be more happy bruh")
+else:
+    print("You're a happy person")
